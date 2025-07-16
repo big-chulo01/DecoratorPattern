76559867dd65a6c60da4453c3b9fb7f73a27f559
Darlington Mboma <darlingtonwealth0213@gmail.com>
2025-07-16 19:00:23 +0000

    decorator and program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create the base component
+        var component = new ConcreteComponent();
+        Console.WriteLine("Base component: " + component.GetText());
+
+        // Create decorators
+        var plainDecorator = new PlainDecorator(component);
+        Console.WriteLine("Plain decorator: " + plainDecorator.GetText());
+
+        var colorDecorator = new ColorDecorator(component);
+        Console.WriteLine("Color decorator: " + colorDecorator.GetText());
+
+        var upperCaseDecorator = new UpperCaseDecorator(component);
+        Console.WriteLine("Upper case decorator: " + upperCaseDecorator.GetText());
+
+        // Demonstrate stacking decorators
+        var stackedDecorator = new UpperCaseDecorator(
+                                new ColorDecorator(
+                                new PlainDecorator(component)));
+        Console.WriteLine("Stacked decorators: " + stackedDecorator.GetText());
+    }
+}
